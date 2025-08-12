79c614af8ab71234e03d71df2d407ade5a454724
huzhen <2027280373@qq.com>
2025-08-12 21:03:45 +0800

    hadoop mapreduce 添加注释

--- src/main/java/com/hutu/mapreduce/IntSumReducer.java
+++ src/main/java/com/hutu/mapreduce/IntSumReducer.java
@@ -9,6 +9,10 @@ import java.io.IOException;
 public class IntSumReducer extends Reducer<Text, IntWritable, Text, IntWritable> {
     private IntWritable result = new IntWritable();
 
+    /**
+     *
+     * @param key 默认是字典排序
+     */
     public void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
         int sum = 0;
         for (IntWritable val : values) {
--- src/main/java/com/hutu/mapreduce/TokenizerMapper.java
+++ src/main/java/com/hutu/mapreduce/TokenizerMapper.java
@@ -12,6 +12,14 @@ import java.io.IOException;
 public class TokenizerMapper extends Mapper<Object, Text, Text, IntWritable> {
     private final static IntWritable one = new IntWritable(1);
     private Text word = new Text();
+
+    /**
+     *
+     * @param key 偏移量 不是行号
+     * @param value 没一行数据的内容
+     * 输入的键值对转化为临时的键值对
+     * <0, pc,work,nc>  偏移量  某一行的值。 转化为 <pc,1> <work,1> <nc,1>
+     */
     public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
         // 使用逗号作为分隔符
         String[] words = value.toString().split(",");
